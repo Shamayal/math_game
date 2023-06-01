761598995fc73435ba18aafde577653ebfe509e6
Shamayal Syed <shamayalsyed9@gmail.com>
2023-06-01 00:10:01 -0400

    added methods to player class

--- player.rb
+++ player.rb
@@ -1,7 +1,19 @@
 class Player
 
+  attr_accessor :name, :score, :lives
+
   def initialize(name)
     @name = name
+    @lives = 3
+    @score = 0
   end
-  
+
+  def update_score
+    @score += 1
+  end
+
+  def lose_life
+    @lives -= 1
+  end
+
 end
